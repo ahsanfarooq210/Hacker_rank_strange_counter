f8c33a0b39f7eb0513a5f6013ef63a1ac91caef9
Ahsan <ahsanfarooq210@gmail.com>
2020-07-29 09:58:14 +0500

    Add project files.

---
+++ Hacker_rank_strange_counter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacker_rank_strange_counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long t = long.Parse(Console.ReadLine());
+            long n = 3;
+            while (t > n)
+            {
+                t -= n;
+                n *= 2;
+            }
+            Console.WriteLine(n - t + 1);
+        }
+
+    }
+}
